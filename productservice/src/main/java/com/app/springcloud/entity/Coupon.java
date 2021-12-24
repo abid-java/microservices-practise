969b0aba68d63ccf03f61af0ef51feaba391fe84
abmohamm <mohd.92java@gmail.com>
2021-12-23 22:00:02 -0500

    Created Feign-Client

---
+++ productservice/src/main/java/com/app/springcloud/entity/Coupon.java
@@ -0,0 +1,55 @@
+package com.app.springcloud.entity;
+
+import java.math.BigDecimal;
+
+public class Coupon {
+
+	private Long id;
+	private String code;
+	private BigDecimal discount;
+	private String expirationDate;
+
+	public Coupon() {
+		super();
+	}
+
+	public Coupon(Long id, String code, BigDecimal discount, String expirationDate) {
+		super();
+		this.id = id;
+		this.code = code;
+		this.discount = discount;
+		this.expirationDate = expirationDate;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public BigDecimal getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(BigDecimal discount) {
+		this.discount = discount;
+	}
+
+	public String getExpirationDate() {
+		return expirationDate;
+	}
+
+	public void setExpirationDate(String expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+}
